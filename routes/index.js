906214620f2231adc2e93b47b9d27c05798898e9
Adron <adronfan@126.com>
2015-05-18 12:17:46 +0800

    update img

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
-// 流水线
 router.get('/jianli', function(req, res, next) {
   res.render('jianli', {});
 });
 
-// 购买流程
 router.get('/xiangmu', function(req, res, next) {
   res.render('xiangmu', {});
 });
 
-// 购买流程
 router.get('/gongsijianjie', function(req, res, next) {
   res.render('gongsijianjie', {});
 });
 
-// 购买流程
 router.get('/hezuohuoban', function(req, res, next) {
   res.render('hezuohuoban', {});
 });
 
-// 售后
 router.get('/shouhou', function(req, res, next) {
   res.render('shouhou', {});
 });
